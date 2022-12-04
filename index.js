f33c294235b8bc5d62cba17cca57adfa1d2fd567
付志强 <zhiqiangfu6@gmail.com>
2022-12-04 19:19:58 +0800

    🔥 feat: init

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+    rules: {
+        get: {
+            /**
+             * api
+             */
+            create() {
+                console.log('init eslint plugin')
+            },
+
+        }
+    }
+}
